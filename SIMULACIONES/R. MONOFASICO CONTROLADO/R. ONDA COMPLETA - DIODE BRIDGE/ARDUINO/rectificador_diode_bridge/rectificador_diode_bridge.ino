cf69ff300825866e31f81df9e5026206ff8c29d1
bremdows <bremdows@gmail.com>
2025-11-07 18:40:12 -0500

    Pruebas y simulaciones

--- SIMULACIONES/R. MONOFASICO CONTROLADO/R. ONDA COMPLETA - DIODE BRIDGE/ARDUINO/rectificador_diode_bridge/rectificador_diode_bridge.ino
+++ SIMULACIONES/R. MONOFASICO CONTROLADO/R. ONDA COMPLETA - DIODE BRIDGE/ARDUINO/rectificador_diode_bridge/rectificador_diode_bridge.ino
@@ -20,15 +20,15 @@ void setup() {
   attachInterrupt(0, deteccionCruceCero, RISING);
 }
 
-void deteccionCruceCero(){
+void deteccionCruceCero() {
   cruceCero = true;
   // Conteo, se utiliza para contar las señales rectificadas
   // Para el monofasico se tiene un total de señales equivalente a 2 (y tal valor delimita al contador)
   x++;
-  if (x == 3){
+  if (x == 3) {
     x = 1;
   }
-  Serial.println(x),
+  // Serial.println(x),
   digitalWrite(triac, LOW);
   digitalWrite(triac2, LOW);
 }
@@ -36,14 +36,14 @@ void deteccionCruceCero(){
 void loop() {
   // Lectura de POT y realizar una equivalencia entre los valores leidos de 0 a 8.3
   POT = analogRead(A0);
+  Serial.println(POT);
   // Con estos valores se define el tiempo de disparo para los SRC (Tiristores)
   // Freq = 60Hz -> T = 16.66ms & T/2 = 8.33ms que es el tiempo máximo para poder enviar el pulso de activación
   dim = map(POT, 0, 1023, 0, 8.3);
-  
-
+  Serial.println(dim);
   // Envía los pulsos según el valor actual del pot para el tiempo
 
-  if ( x == 1 && cruceCero == true){
+  if (x == 1 && cruceCero == true) {
     delay(dim);
     digitalWrite(triac, HIGH);
     delay(1);
@@ -52,7 +52,7 @@ void loop() {
   }
 
 
-  if ( x == 2 && cruceCero == true){
+  if (x == 2 && cruceCero == true) {
     delay(dim);
     digitalWrite(triac2, HIGH);
     delay(1);
