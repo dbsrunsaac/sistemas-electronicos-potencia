adcf84029730247141e82db361f11323845fdad1
bremdows <bremdows@gmail.com>
2025-11-10 10:31:20 -0500

    Aplicación cruce por cero

--- SIMULACIONES/DIMMER/ARDUINO/switchLamp/switchLamp.ino
+++ SIMULACIONES/DIMMER/ARDUINO/switchLamp/switchLamp.ino
@@ -1,27 +1,39 @@
-int btnSwitch = 2;
+// volatile se usa para definir una variable a ser usada en una
+volatile boolean cruceCero = false;
+
+int numeroCruces = 0;
 int lamp = 3;
 
-int stateButton;
-bool stateLamp = false;
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
-  pinMode(btnSwitch, INPUT);
-  pinMode(lamp, 3);
+
+  // Pin de salida
+  pinMode(lamp, OUTPUT);
+
+  // Definir el pin 2 para la interrupción
+  attachInterrupt(digitalPinToInterrupt(2), deteccionCruceCero, RISING);
+
   Serial.println("Interruptor inteligente iniciado");
+  Serial.println("Parpadero cada 0.5s");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  stateButton = digitalRead(btnSwitch);
-  if (stateButton == HIGH){
-    Serial.println("Boton presionado");
-    if (stateLamp){
-      digitalWrite(lamp, LOW);
-      stateLamp = false;
-    }else{
-      digitalWrite(lamp, HIGH);
-      stateLamp = true;
-    }
+  // Código para la implementación
+  if (numeroCruces == 60 ){
+    digitalWrite(lamp, HIGH);
+    delay(1);
+    digitalWrite(lamp, LOW);
+    cruceCero = false;
+    Serial.println("Lampara prendida");
   }
+}
+
+void deteccionCruceCero(){
+  cruceCero = true;
+  numeroCruces++;
+  if (numeroCruces == 61){
+    numeroCruces = 0;
+  }
+  Serial.println(numeroCruces);
 }
--- SIMULACIONES/R. MONOFASICO CONTROLADO/R. ONDA COMPLETA - DIODE BRIDGE V2/ARDUINO/disparo_pulsos/disparo_pulsos.ino
+++ SIMULACIONES/R. MONOFASICO CONTROLADO/R. ONDA COMPLETA - DIODE BRIDGE V2/ARDUINO/disparo_pulsos/disparo_pulsos.ino
@@ -4,6 +4,8 @@ volatile boolean cruceCero = false;
 // Variables de salida
 int p1 = 3;
 int p2 = 4;
+int p3 = 5;
+int p4 = 6;
 
 // Adicionales
 int dim = 0;
@@ -18,33 +20,39 @@ void setup() {
   // Salida
   pinMode(p1, OUTPUT);
   pinMode(p2, OUTPUT);
-
+  pinMode(p3, OUTPUT);
+  pinMode(p4, OUTPUT);
   // Asincronismo
   attachInterrupt(0, deteccionCruceCero, RISING);
 
-  Serial.print("Rectificador Monofasico Controlando..");
-  Serial.print("Programa Iniciado");
+  Serial.println("Rectificador Monofasico Controlando..");
+  Serial.println("Programa Iniciado");
 }
 
 void loop() {
   
   // Iniciando
   pot = analogRead(A0);
+  dim = map(pot, 0, 1023, 0, 500);
 
-  dim = map(pot, 0, 1023, 0, 8.3);
   if (x == 1 && cruceCero == true){
     delay(dim);
     digitalWrite(p1, HIGH);
+    digitalWrite(p3, HIGH);
     delay(1);
     digitalWrite(p1, LOW);
+    digitalWrite(p3, LOW);
     cruceCero = false;
   }
 
   if (x == 2 && cruceCero == true){
     delay(dim);
+    digitalWrite(p4, HIGH);
     digitalWrite(p2, HIGH);
     delay(1);
+    digitalWrite(p4, LOW);
     digitalWrite(p2, LOW);
+    
     cruceCero = false;
   }
 
@@ -60,4 +68,7 @@ void deteccionCruceCero(){
   Serial.println(x);
   digitalWrite(p1, LOW);
   digitalWrite(p2, LOW);
+  digitalWrite(p3, LOW);
+  digitalWrite(p4, LOW);
 }
+
